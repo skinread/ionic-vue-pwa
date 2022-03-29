e2b8a9fcf4c1d93ade16205528a92309f6824854
Sean Kinread <sean@kinread.com>
2022-03-29 15:58:11 +1100

    add pwa

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 import router from './router';
+import './registerServiceWorker';
 
 import { IonicVue } from '@ionic/vue';
 
@@ -23,10 +24,8 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
-const app = createApp(App)
-  .use(IonicVue)
-  .use(router);
-  
+const app = createApp(App).use(IonicVue).use(router);
+
 router.isReady().then(() => {
   app.mount('#app');
 });
